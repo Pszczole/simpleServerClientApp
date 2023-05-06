58788d393cd21fb5e7b88bd7c9e6be4e32ec09c0
Pszczole <cezary932@gmail.com>
2023-05-06 09:31:21 +0200

    Updated Client and Server.

--- src/Client.java
+++ src/Client.java
@@ -3,18 +3,27 @@ import java.net.*;
 import java.util.Scanner;
 
 public class Client {
+    private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+    public void startConnection(String ip, int port) throws IOException {
+        clientSocket = new Socket(ip, port);
+        out = new PrintWriter(clientSocket.getOutputStream());
+        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+    }
+
+    public void sendIp(){
+        Scanner clientInput = new Scanner(System.in);
+        System.out.println("Enter user id!");
+        out.write(clientInput.nextInt());
+        out.flush();
+    }
+
     public static void main(String[] args) throws IOException {
 
-            Socket s = new Socket("localhost",4951);
-
-
-            Scanner clientInput = new Scanner(System.in);
-
-            System.out.println("Enter user id!");
-
-            PrintWriter pr = new PrintWriter(s.getOutputStream());
-            pr.write(clientInput.nextInt());
-            pr.flush();
+            Client client = new Client();
+            client.startConnection("localhost",4951);
+            client.sendIp();
 
     }
 }
--- src/Server.java
+++ src/Server.java
@@ -1,9 +1,36 @@
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
+    private ServerSocket serverSocket;
+    private Socket clientSocket;
+    private PrintWriter out;
+    private InputStreamReader in;
+    public void start(int port) throws IOException {
+
+        serverSocket = new ServerSocket(port);
+        clientSocket = serverSocket.accept();
+        in = new InputStreamReader(clientSocket.getInputStream());
+        out = new PrintWriter(clientSocket.getOutputStream());
+        System.out.println("Client connected ");
+    }
+
+    public void getId() throws IOException {
+        int id = in.read();
+        System.out.println("Client id:"+id);
+    }
+
+    public void stop() throws IOException {
+        in.close();
+        out.close();
+        clientSocket.close();
+        serverSocket.close();
+    }
+
     public static void main(String[] args) throws IOException {
 
         DbConnection db = new DbConnection();
@@ -11,17 +38,9 @@ public class Server {
         //Establish  connection with our database
         db.connect_to_db("insuranceCompany","postgres","root");
 
-        ServerSocket ss = new ServerSocket(4951);
-        Socket s = ss.accept();
-
-        System.out.println("Client connected ");
-
-        InputStreamReader in = new InputStreamReader(s.getInputStream());
-        int id = in.read();
-
-        System.out.println("Client id:"+id);
-
-
+        Server server = new Server();
+        server.start(4951);
+        server.getId();
 
     }
 }
